25cedf6a0e000681211761446f1a8d45d666e5bd
Spartan-0 <126047640+Spartan-0@users.noreply.github.com>
2023-02-22 17:44:50 +0530

    Create Pyspark_df_plotting.py

---
+++ Pyspark_df_plotting.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from pyspark.sql.functions import *
+
+# Create a PySpark DataFrame
+df = spark.createDataFrame([(1, 2), (3, 4), (5, 6)], ["col1", "col2"])
+
+# Convert PySpark DataFrame to Pandas DataFrame
+pandas_df = df.toPandas()
+
+# Generate a bar chart
+pandas_df.plot(kind="bar", x="col1", y="col2")
+plt.show()
+
+# Generate a scatter plot
+pandas_df.plot(kind="scatter", x="col1", y="col2")
+plt.show()
